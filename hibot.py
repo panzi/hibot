91582d86445b0ec84eb0b14e94012d273fb4d3ab
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2016-01-10 03:10:53 +0100

    initial commit

---
+++ hibot.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import re
+import irc.bot
+from time import time
+
+if hasattr(__builtins__, 'xrange'):
+	range = xrange
+
+RE_GENERAL_GREETING = re.compile(
+	r'^\s*(?:feliciaBoom\s+)*'
+	r'(?:hi+|hey|hia|heya|hello|greetings|howdy)'
+	r'(?:\s+(?:all|everyone|everybody|weirdos|suckers|hoomans|people|ppl|again))?\s*'
+	r'[\.!]*(?:\s+feliciaBoom)*\s*$', re.I)
+
+RE_GREETING = re.compile(
+	r'^\s*(?:feliciaBoom\s+)*'
+	r'(?:hi|hey|hia|heya|hello|greetings|howdy)\s+'
+	r'(.*?)\s*$', re.I)
+
+def normalize_nick(alias):
+	return alias.strip().lower()
+
+class HiBot(irc.bot.SingleServerIRCBot):
+	def __init__(self, nickname, channels, nickalias=None, password=None, server='irc.twitch.tv', port=6667, greet_timeout=3600):
+		irc.bot.SingleServerIRCBot.__init__(self, [(server, port, password)], nickname, nickname)
+		self.join_channels = [channel if channel.startswith('#') else '#'+channel for channel in channels]
+		self.nickalias     = set(tuple(normalize_nick(nick).split()) for nick in nickalias) if nickalias is not None else set()
+		self.greeted       = {}
+		self.greet_timeout = greet_timeout
+
+		norm_nick = normalize_nick(nickname)
+		self.nickalias.add((norm_nick,))
+		self.nickalias.add(('@'+norm_nick,))
+
+	def on_welcome(self, connection, event):
+		for channel in self.join_channels:
+			connection.join(channel)
+
+	def on_nicknameinuse(self, connection, event):
+		print('error: nickname in use')
+
+	def on_error(self, connection, event):
+		print('error: %s' % ' '.join(event.arguments))
+
+	def on_pubmsg(self, connection, event):
+		norm_nick   = normalize_nick(self.connection.get_nickname())
+		sender      = event.source.nick
+		norm_sender = normalize_nick(sender)
+		if norm_sender != norm_nick and time() - self.greeted.get(norm_sender, 0) > self.greet_timeout:
+			message = event.arguments[0]
+
+			match = RE_GENERAL_GREETING.match(message)
+			if match:
+				self._say_hi(sender, event.target)
+				return
+
+			match = RE_GREETING.match(message)
+			if match:
+				nicks = normalize_nick(match.group(1)).replace(',',' ').split()
+				if self._contains_alias(nicks):
+					self._say_hi(sender, event.target)
+					return
+
+	def _contains_alias(self, nicks):
+		for alias in self.nickalias:
+			alias_len = len(alias)
+			for i in range(0,len(nicks),alias_len):
+				if alias == tuple(nicks[i:i+alias_len]):
+					return True
+		return False
+
+	def _say_hi(self, sender, channel):
+		self.connection.privmsg(channel, "Hi @%s!" % sender)
+		self.greeted[normalize_nick(sender)] = time()
+
+def main(args):
+	import yaml
+	import argparse
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-c','--config',default='config.yaml')
+	opts = parser.parse_args(args)
+
+	with open(opts.config,'rb') as fp:
+		config = yaml.load(fp)
+
+	server, port = config.get('host','irc.twitch.tv:6667').split(':',1)
+	port = int(port)
+	greet_timeout = config.get('greet_timeout',3600)
+
+	bot = HiBot(
+		config['nickname'],
+		config['channels'],
+		config.get('nickalias'),
+		config.get('password'),
+		server,
+		port,
+		greet_timeout)
+	bot.start()
+
+if __name__ == '__main__':
+	import sys
+
+	try:
+		main(sys.argv[1:])
+	except KeyboardInterrupt:
+		print()
