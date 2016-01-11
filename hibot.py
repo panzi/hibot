c0e101a41770c8eb1a9c6b993d413d2ed1945754
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2016-01-11 03:17:01 +0100

    fix crash caused by regex

--- hibot.py
+++ hibot.py
@@ -12,7 +12,7 @@ if hasattr(__builtins__, 'xrange'):
 FLOURISH = r'(?:feliciaBoom|<3|gandsLessThanThree)'
 
 GREETING = \
-	r"(?:hi+|hey+|hia+|hi-?ya+|heya+|heyo+|h[eau]llo+|greetings+|howdy+|welcome(?:\s+back)?|(?:what)?'?s\s*up(?:\s*dog+)|what\s*up(?:\s*dog+)?|howdy-?do+|yo|wh?add?\s*up(\s*dog+)?|yuhu+|good\s*day|'?g\s*day)"
+	r"(?:hi+|hey+|hia+|hi-?ya+|heya+|heyo+|h[eau]llo+|greetings+|howdy+|welcome(?:\s+back)?|(?:what)?'?s\s*up(?:\s*dog+)|what\s*up(?:\s*dog+)?|howdy-?do+|yo|wh?add?\s*up(?:\s*dog+)?|yuhu+|good\s*day|'?g\s*day)"
 
 RE_GENERAL_GREETING = re.compile(
 	r'^\s*(?:' + FLOURISH + '\s+)*' + GREETING +
