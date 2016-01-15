be01388b328bd2b10d58d60edd7a9dc8681f6b9a
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2016-01-16 00:29:09 +0100

    also recognize "hai" as greeting

--- hibot.py
+++ hibot.py
@@ -14,7 +14,7 @@ if hasattr(__builtins__, 'xrange'):
 FLOURISH = r'(?:feliciaBoom|<3|gandsLessThanThree)'
 
 GREETING = \
-	r"(?:also,?\s+)?(?:hi+|hey+|heys+|hia+|hi-?ya+|heya+|heyo+|h[eau]llo+|howdy+doo+dy+|greetings+|howdy+|welcome(?:\s+back)?|(?:what)?'?s\s*up(?:\s*dog+)?|what\s*up(?:\s*dog+)?|howdy-?do+|yo+|wh?add?\s*up(?:\s*dog+)?|yuhu+|good\s*day|'?g\s*day)(?:\s+to)?"
+	r"(?:also,?\s+)?(?:ha*i+|hey+|heys+|hia+|hi-?ya+|heya+|heyo+|h[eau]llo+|howdy+doo+dy+|greetings+|howdy+|welcome(?:\s+back)?|(?:what)?'?s\s*up(?:\s*dog+)?|what\s*up(?:\s*dog+)?|howdy-?do+|yo+|wh?add?\s*up(?:\s*dog+)?|yuhu+|good\s*day|'?g\s*day)(?:\s+to)?"
 
 RE_GENERAL_GREETING = re.compile(
 	r'^\s*(?:' + FLOURISH + '\s+)*' + GREETING +
